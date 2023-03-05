f2fed52726ff60cdd60ed0a73e88db62f0d0ff40
Joe Turner <joe@agavemountain.com>
2023-03-05 12:28:31 -0600

    Update readme.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Common Types
 
-This header-only repository contains common types, equates, defines and 
-specialized template functions. 
+This header-only repository contains common types, structures, equates, defines and 
+specialized template functions.
+
+To put it bluntly, this repository contains code that other code depends on so it 
+isn't a stand alone repository.  We are slowly refactoring and moving code into
+this repository. Use at your own risk.
+
--- include/namespaces.hpp
+++ include/namespaces.hpp
@@ -3,46 +3,54 @@
 //! This file contains doxygen documentation for the specified
 //! namespaces included in the project.
 //!
-//! \copyright 
+//! \copyright
 //!
 //! This file is part of the flip distribution (https://github.com/xxxx or http://xxx.github.io).
 //! Copyright (c) 2023 Joe Turner.
-//! 
-//! This program is free software: you can redistribute it and/or modify  
-//! it under the terms of the GNU General Public License as published by  
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
 //! the Free Software Foundation, version 3.
 //!
-//! This program is distributed in the hope that it will be useful, but 
-//! WITHOUT ANY WARRANTY; without even the implied warranty of 
-//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+//! This program is distributed in the hope that it will be useful, but
+//! WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 //! General Public License for more details.
 //!
-//! You should have received a copy of the GNU General Public License 
+//! You should have received a copy of the GNU General Public License
 //! along with this program. If not, see <http://www.gnu.org/licenses/>.
 #ifndef _IOI_NAMESPACES_HPP_
 #define _IOI_NAMESPACES_HPP_
 
 //! \brief IO Industrial Namespace
-namespace ioi {
-
-//! \brief Transporation
-//! This namespace contains formulas, enumerations, constants, and
-//! inline code relating to transportation processes.
-namespace transportation
+namespace ioi
 {
 
-};
+    //! \brief Transporation
+    //! This namespace contains formulas, enumerations, constants, and
+    //! inline code relating to transportation processes.
+    namespace transportation
+    {
 
-//! \brief Accounting
-//! This namespace contains formulas, enumerations, constants, and
-//! inline code relating to accounting processes.
-//!
-//! More specifically, this namespace contains code to calculate payroll taxes,
-//! witholding, and otherwise generate payroll checks.
-namespace accounting
-{
+    };
 
-}
+    //! \brief Accounting
+    //! This namespace contains formulas, enumerations, constants, and
+    //! inline code relating to accounting processes.
+    //!
+    //! More specifically, this namespace contains code to calculate payroll taxes,
+    //! witholding, and otherwise generate payroll checks.
+    namespace accounting
+    {
+
+        //! \brief Payroll
+        //! This namespace contains formuulas, enumerations, constants, and
+        //! inline code relating to payroll operations.
+        namespace payroll
+        {
+
+        }
+    }
 
 };
 
